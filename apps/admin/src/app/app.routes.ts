6bfc26c8a48745fbf580ee35167e5b445dcd1285
Vitaliy <VitaliyThupin@gmail.com>
2026-06-10 20:04:52 +0300

    feat(admin): mount references routes

--- apps/admin/src/app/app.routes.ts
+++ apps/admin/src/app/app.routes.ts
@@ -1,4 +1,11 @@
 import { dashboardRoutes } from '@admin/feature/dashboard';
+import { referencesRoutes } from '@admin/feature/references';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [...dashboardRoutes];
+export const appRoutes: Route[] = [
+  ...dashboardRoutes,
+  {
+    path: 'references',
+    children: referencesRoutes,
+  },
+];
